326c5c67ef917d98b542eae3190330c25b0634e3
smartht2 <info@smartht.ru>
2023-09-21 18:22:54 +0500

    demo

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 "use strict";
+var Cron = require('croner');
+var child_process = require('child_process');
 module.exports = function (homebridge) {
 	console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!2222222222222222222222");
-};
+	const job = Cron('*/5 * * * * *', () => {
+	});
+	setTimeout(() => {
+		console.log('Fallback. Soft restarting now (pkill -15 homebridge)');
+        const kill = child_process.spawn('pkill', ['-15', 'homebridge']);
+        kill.stdout.on('data', (data) => {
+        	console.log(data.toString());
+        });
+        kill.stderr.on('data', (data) => {
+        	console.log(data.toString());
+		});
+	}, 30000);
+}; 
